74a8b33b0ab977515d9a679e478ea2497e8f8ce9
junho3168 <junho3168@naver.com>
2024-11-19 14:34:49 +0900

    Scanner

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,5 +1,7 @@
 package com.ll;
 
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -14,6 +16,16 @@ class App {
     public void run() {
         System.out.println("== 명언 앱 ==");
 
+        //스캐너 생성 (키보드 입력 스캔, 파이썬 인풋같은거)
+        Scanner scanner = new Scanner(System.in);
+
+
         System.out.print("명령) ");
+        String cmd = scanner.nextLine();
+
+        System.out.println("입력된 명령어 : %s".formatted(cmd));
+
+        scanner.close();
+        System.out.println("종료합니다");
     }
 }
